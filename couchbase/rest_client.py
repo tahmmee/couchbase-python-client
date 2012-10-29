62645c1fac3446be6b178c0901e2976cae95ea3f
Tommie McAfee <tommie@couchbase.com>
2012-10-29 14:12:08 -0400

    181 compatibility

--- couchbase/rest_client.py
+++ couchbase/rest_client.py
@@ -1051,8 +1051,15 @@ class RestParser(object):
         bucket.port = parsed['proxyPort']
         bucket.authType = parsed["authType"]
         bucket.saslPassword = parsed["saslPassword"]
-        bucket.controllers = parsed['controllers']
-        bucket.ddocs = parsed['ddocs']
+        if 'controllers' in parsed:
+            bucket.controllers = parsed['controllers']
+        else:
+            bucket.controllers = []
+        if 'ddocs' in parsed:
+            bucket.ddocs = parsed['ddocs']
+        else:
+            bucket.ddocs = []
+
         bucket.nodes = list()
         if 'vBucketServerMap' in parsed:
             vBucketServerMap = parsed['vBucketServerMap']
